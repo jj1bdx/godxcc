e26fcd15df4bd326824518d05159575df8d9201f
Kenji Rikitake <kenji.rikitake@acm.org>
2022-10-12 21:52:55 +0900

    Revise main/main.go

--- main/main.go
+++ main/main.go
@@ -1,9 +1,19 @@
 package main
 
 import (
+	"fmt"
 	"github.com/jj1bdx/godxcc"
 )
 
+func printinfo(call string) {
+	fmt.Printf("call: %s, DXCCData: %v\n", call, godxcc.DXCCGetRecord(call))
+}
+
 func main() {
 	godxcc.LoadCty()
+	printinfo("jj1bdx")
+	printinfo("8J1RL")
+	printinfo("KL7/JJ1BDX")
+	printinfo("W1AW/KH0")
+	printinfo("3D2AG")
 }
